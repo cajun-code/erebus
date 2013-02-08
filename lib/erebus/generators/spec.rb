13aa7889c8f735d641365830731b288c6bfa3ac2
Allan Davis <cajun.code@gmail.com>
2013-02-08 00:15:56 -0500

    create spec generator

--- lib/erebus/generators/spec.rb
+++ lib/erebus/generators/spec.rb
@@ -4,16 +4,20 @@ class Spec < Erebus::NamedGenerator
   desc "Create a Igloo testing source file"
   
   #class_option :ext,:type => :string ,:default => "cpp", :desc => "Extention used for the source file"
-  
-  #validate_project
-  
+ 
+  class_option :use_alt, :type => :boolean, :default => true, :desc => "Use the alt syntax for igloo"
+ 
   def self.source_root
     File.dirname(__FILE__)
   end
   
   def create_source_file
     Spec.validate_project
-    template "templates/spec.erb", "spec/#{file_name}.#{ext}"
+    if(options[:use_alt])
+      template "templates/spec_alt.erb", "spec/#{file_name}_spec.#{ext}"
+    else
+      template "templates/spec.erb", "spec/#{file_name}_spec.#{ext}"
+    end
   end
   
   private
